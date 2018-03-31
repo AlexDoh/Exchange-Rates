c1cc974e8ae5836c432aeaeeafdf7f634ec852e8
Alex <odmitrenko1@gmail.com>
2018-04-01 01:28:02 +0300

    Changed project to using Spring Boot.

--- exchange-server/src/main/java/com/odmytrenko/controller/HomeController.java
+++ exchange-server/src/main/java/com/odmytrenko/controller/HomeController.java
@@ -3,12 +3,13 @@ package com.odmytrenko.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String homePage(){
-        return "index";
-    }
+//    @RequestMapping(path = "/", method = RequestMethod.GET)
+//    public ModelAndView homePage(){
+//        return new ModelAndView("index.html");
+//    }
 }
