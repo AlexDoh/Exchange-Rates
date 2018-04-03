9ce71084b8761252909b30a643c1c831c67aab92
Alex <odmitrenko1@gmail.com>
2018-04-04 02:22:46 +0300

    Added logic for returning correct info for finance service.

--- exchange-server/src/main/java/com/odmytrenko/service/FinanceExchangeService.java
+++ exchange-server/src/main/java/com/odmytrenko/service/FinanceExchangeService.java
@@ -1,7 +1,6 @@
 package com.odmytrenko.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.odmytrenko.dto.ResponseExchangeInfo;
 import com.odmytrenko.model.ExchangeProvider;
 import com.odmytrenko.model.finance.FinanceProviderInfo;
 import org.jsoup.Jsoup;
@@ -16,17 +15,13 @@ public class FinanceExchangeService implements ExchangeService {
     @Override
     public ExchangeProvider getExchangeProviderInfo() {
         try {
-            String json = Jsoup.connect("http://resources.finance.ua/ru/public/currency-cash.json").ignoreContentType(true).execute().body();
+            String json = Jsoup.connect("http://resources.finance.ua/ru/public/currency-cash.json")
+                .ignoreContentType(true).execute().body();
             ObjectMapper mapper = new ObjectMapper();
-            FinanceProviderInfo response = mapper.readValue(json, FinanceProviderInfo.class);
+            return mapper.readValue(json, FinanceProviderInfo.class);
         } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException("There was an error during parse json object: " + e.getMessage(), e.getCause());
         }
-        return null;
-    }
-
-    @Override
-    public ResponseExchangeInfo getExchangeInfo() {
-        return null;
     }
 }
