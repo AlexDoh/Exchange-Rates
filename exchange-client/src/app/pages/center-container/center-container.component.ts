b82cb81133db2d454bb12fb8ae9690c99ee0a16b
Alex <odmitrenko1@gmail.com>
2018-04-10 00:49:48 +0300

    Code clean.

--- exchange-client/src/app/pages/center-container/center-container.component.ts
+++ exchange-client/src/app/pages/center-container/center-container.component.ts
@@ -1,25 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ExchangeService } from '../../shared/services/exchange.service';
 import { ExchangeInfo } from '../../models/exchange-info';
-import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-center-container',
   templateUrl: './center-container.component.html',
-  styleUrls: [ './center-container.component.scss' ],
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    multi: true,
-    useExisting: CenterContainerComponent
-  }]
+  styleUrls: [ './center-container.component.scss' ]
 })
-export class CenterContainerComponent implements OnInit, ControlValueAccessor {
+export class CenterContainerComponent implements OnInit {
 
   exchangeData: ExchangeInfo = new ExchangeInfo();
   title: string = 'Finance';
   selectedCurrency: string;
   selectCurrencyForm: FormGroup;
-  kolo: any;
 
   constructor(
     private exchangeService: ExchangeService,
@@ -29,17 +23,12 @@ export class CenterContainerComponent implements OnInit, ControlValueAccessor {
   ngOnInit() {
     this.requestExchangeData();
     this.initForm();
-    this.yolo();
     this.selectedCurrency = 'USD';
   }
 
   requestExchangeData(): void {
     this.exchangeService.getFinanceExchangeInfo().subscribe(result => this.exchangeData = result);
   };
-
-  yolo() {
-    console.log(this.selectCurrencyForm.value);
-  }
 
   initForm() {
     this.selectCurrencyForm = this.fb.group({
@@ -50,25 +39,4 @@ export class CenterContainerComponent implements OnInit, ControlValueAccessor {
       this.selectedCurrency = value.currency;
     });
   }
-
-  getCurrency() {
-    return this.selectedCurrency;
-  }
-
-  registerOnChange(fn: any): void {
-    this.kolo = fn;
-  }
-
-  registerOnTouched(fn: any): void {
-    this.kolo = fn;
-  }
-
-
-  setDisabledState(isDisabled: boolean): void {
-    console.log(isDisabled);
-  }
-  writeValue(obj: any): void {
-    console.log(obj);
-  }
-
 }
