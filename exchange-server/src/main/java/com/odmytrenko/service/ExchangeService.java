78b703b4654a8b382039c1d0b34989f3b684a1e3
Alex <odmitrenko1@gmail.com>
2018-05-06 14:04:59 +0300

    Removed unused.

--- exchange-server/src/main/java/com/odmytrenko/service/ExchangeService.java
+++ exchange-server/src/main/java/com/odmytrenko/service/ExchangeService.java
@@ -5,17 +5,4 @@ import com.odmytrenko.model.ExchangeProvider;
 public interface ExchangeService {
 
     ExchangeProvider getExchangeProviderInfo();
-
-//    default ExchangeProvider getExchangeInfo() {
-//        ExchangeProvider exchangeProvider = this.getExchangeProviderInfo();
-//
-//        exchangeProvider.setOrgTypes(
-//            Optional.ofNullable(exchangeProvider.getOrgTypes()).orElseGet(() -> BankConstants.ORG_TYPES)
-//        );
-//        exchangeProvider.setCurrencies(
-//            Optional.ofNullable(exchangeProvider.getCurrencies()).orElseGet(() -> BankConstants.CURRENCIES)
-//        );
-//
-//        return exchangeProvider;
-//    }
 }
