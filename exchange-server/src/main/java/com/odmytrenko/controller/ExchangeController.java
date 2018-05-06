6dabd0a5ea4a6cf69b644c704ffb120b1bf85f2b
Alex <odmitrenko1@gmail.com>
2018-05-06 09:49:44 +0300

    Added logger to fin service.

--- exchange-server/src/main/java/com/odmytrenko/controller/ExchangeController.java
+++ exchange-server/src/main/java/com/odmytrenko/controller/ExchangeController.java
@@ -3,8 +3,6 @@ package com.odmytrenko.controller;
 import com.odmytrenko.dao.ExchangeProviderRepository;
 import com.odmytrenko.model.ExchangeProvider;
 import com.odmytrenko.service.ExchangeService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
@@ -19,8 +17,6 @@ import java.util.Optional;
 @RestController
 @RequestMapping(path = "/rates")
 public class ExchangeController {
-
-    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("kurs")
@@ -43,10 +39,6 @@ public class ExchangeController {
     @RequestMapping(path = "/finance", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<ExchangeProvider> getFinanceExchangeInfo() {
-        LOG.debug("This is a debug message");
-        LOG.info("This is an info message");
-        LOG.warn("This is a warn message");
-        LOG.error("This is an error message");
         ExchangeProvider exchangeProvider = exchangeProviderRepository.findByTitle("Finance.ua");
         return Optional.of(ResponseEntity.ok(exchangeProvider)).orElse(ResponseEntity.notFound().build());
     }
--- exchange-server/src/main/java/com/odmytrenko/service/FinanceExchangeService.java
+++ exchange-server/src/main/java/com/odmytrenko/service/FinanceExchangeService.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.odmytrenko.configuration.ProvidersProperties;
 import com.odmytrenko.model.finance.FinanceProviderInfo;
 import org.jsoup.Jsoup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,13 +16,17 @@ import java.io.IOException;
 @Qualifier("finance")
 public class FinanceExchangeService implements ExchangeService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FinanceExchangeService.class);
+    private static final String URL = "http://resources.finance.ua/ru/public/currency-cash.json";
+
     @Autowired
     ProvidersProperties providersProperties;
 
     @Override
     public FinanceProviderInfo getExchangeProviderInfo() {
+        LOG.info("Requesting provider info from {}", URL);
         try {
-            String json = Jsoup.connect("http://resources.finance.ua/ru/public/currency-cash.json")
+            String json = Jsoup.connect(URL)
                 .ignoreContentType(true).execute().body();
             ObjectMapper mapper = new ObjectMapper();
             FinanceProviderInfo financeProviderInfo = mapper.readValue(json, FinanceProviderInfo.class);
