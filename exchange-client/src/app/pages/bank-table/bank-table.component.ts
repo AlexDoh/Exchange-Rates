9d3cb2a54e0d35fbda1935f2df0ecd53969b9540
AlexDoh <odmitrenko1@gmail.com>
2018-05-07 19:30:44 +0300

    Added highlighting min value.

--- exchange-client/src/app/pages/bank-table/bank-table.component.ts
+++ exchange-client/src/app/pages/bank-table/bank-table.component.ts
@@ -1,26 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ExchangeInfo } from '../../models/exchange-info';
 
 @Component({
   selector: 'app-bank-table',
   templateUrl: './bank-table.component.html',
-  styleUrls: ['./bank-table.component.scss']
+  styleUrls: [ './bank-table.component.scss' ]
 })
-export class BankTableComponent implements OnInit {
+export class BankTableComponent implements OnInit, OnChanges {
 
   @Input() exchangeData: ExchangeInfo;
   @Input() currencyType: string;
   isDesc: boolean = false;
   column: string = 'title';
+  minAsk: number = Number.MAX_SAFE_INTEGER;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-  sort(columnName){
+  sort(columnName) {
     this.isDesc = !this.isDesc;
     this.column = columnName;
   };
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.exchangeData.currentValue) {
+      this.setMinAskCurrency(changes.exchangeData.currentValue);
+    }
+  }
+
+  setMinAskCurrency(exchangeData: ExchangeInfo): void {
+    exchangeData.organizations.forEach(organization => {
+      Object.entries(organization.currencies).filter(([ currencyType ]) =>
+        currencyType === this.currencyType).forEach(([ currencyType, currencyData ]) => {
+        const ask = parseFloat(currencyData.ask);
+        if (ask < this.minAsk) {
+          this.minAsk = ask;
+        }
+      })
+    });
+  }
 
 }
