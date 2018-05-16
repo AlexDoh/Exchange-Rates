dfbb72ce277290614dcbfc45c68b32198ac761b2
AlexDoh <odmitrenko1@gmail.com>
2018-05-16 17:11:44 +0300

    Fixed die to a new rxjs version.

--- exchange-client/src/app/shared/services/rest/exchange.service.ts
+++ exchange-client/src/app/shared/services/rest/exchange.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+
 import { environment } from '../../../../environments/environment';
 import { ExchangeInfo } from '../../../models/exchange-info';
+import { map } from "rxjs/operators";
 
 @Injectable()
 export class ExchangeService {
@@ -12,11 +13,11 @@ export class ExchangeService {
   }
 
   getFinanceExchangeInfo(): Observable<ExchangeInfo> {
-    return this.http.get<ExchangeInfo>(environment.api.exchange.get.finance).map(result => new ExchangeInfo(result));
+    return this.http.get<ExchangeInfo>(environment.api.exchange.get.finance).pipe(map(result => new ExchangeInfo(result)));
   }
 
   getKursExchangeInfo(): Observable<ExchangeInfo> {
-    return this.http.get<ExchangeInfo>(environment.api.exchange.get.kurs).map(result => new ExchangeInfo(result));
+    return this.http.get<ExchangeInfo>(environment.api.exchange.get.kurs).pipe(map(result => new ExchangeInfo(result)));
   }
 
 }
