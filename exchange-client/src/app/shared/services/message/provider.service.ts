3702edbfcf2195e24ac13288be3565e0c2cdab3f
AlexDoh <odmitrenko1@gmail.com>
2018-05-16 11:21:20 +0300

    Added new logic to services.

--- exchange-client/src/app/shared/services/message/provider.service.ts
+++ exchange-client/src/app/shared/services/message/provider.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Subject } from "rxjs/Subject";
+import { ExchangeInfo } from "../../../models/exchange-info";
 
 @Injectable()
 export class ProviderService {
@@ -7,6 +9,8 @@ export class ProviderService {
   private defaultCurrency = 'USD';
   private currencySource = new BehaviorSubject<string>(this.defaultCurrency);
   currentCurrency = this.currencySource.asObservable();
+
+  subjectProvider = new Subject<ExchangeInfo>();
 
   constructor() { }
 
